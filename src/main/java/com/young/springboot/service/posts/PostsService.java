a693a9b16efc5499ddf32edcee06472ef7bb8757
yunyoung1819 <yunyoung1819@hanmail.net>
2021-09-20 13:53:27 +0900

    # Posts 등록 기능 구현

---
+++ src/main/java/com/young/springboot/service/posts/PostsService.java
@@ -0,0 +1,18 @@
+package com.young.springboot.service.posts;
+
+import com.young.springboot.domain.posts.PostsRepository;
+import com.young.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
