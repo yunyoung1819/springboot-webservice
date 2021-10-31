ec9620339cb1eeb829b7fc26723f06167def591a
yyun <yyun@nkia.co.kr>
2021-11-01 00:27:37 +0900

    [ADD]: 네이버 소셜 로그인 기능 추가

--- src/main/java/com/young/springboot/web/IndexController.java
+++ src/main/java/com/young/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.young.springboot.web;
 
+import com.young.springboot.config.auth.LoginUser;
 import com.young.springboot.config.auth.dto.SessionUser;
 import com.young.springboot.service.posts.PostsService;
 import com.young.springboot.web.dto.PostsResponseDto;
@@ -19,10 +20,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if (user != null) {
             model.addAttribute("userName", user.getName());
